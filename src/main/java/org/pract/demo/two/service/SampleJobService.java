be4ea16ce7056db62e0144d0d265daad3f4f8ceb
Deepak Bhoria <deepak.bhoria@rategain.com>
2020-07-11 14:35:34 +0530

    logging through @Slf4j annotation

--- src/main/java/org/pract/demo/two/service/SampleJobService.java
+++ src/main/java/org/pract/demo/two/service/SampleJobService.java
@@ -1,12 +1,15 @@
 package org.pract.demo.two.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class SampleJobService {
     public void executeSampleJob() {
         try {
-            System.out.println("inside sample job");
+//            System.out.println("inside sample job");
+            log.info("logging through sample job");
             Thread.sleep(5000);
         } catch (InterruptedException e) {
         } finally {
